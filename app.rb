44fb54af52aaa074a43de8ed5b59b7b5ff5d173a
deepakmdass88 <deepak@deeproot.in>
2012-12-19 15:21:12 +0530

    Adding condition for including the custom file into local.cf and generating the custom file once the session starts

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 #! /usr/bin/ruby
 require 'rubygems'
 require 'choice'
+require 'fileutils'
+##### Basic Varibles #####
+@filename = '/etc/spamassassin/thor.cf' 
+@sa_conf  = '/etc/spamassassin/local.cf'
 
+##### Defining CLI Options #####
 Choice.options do
   header ''
   header 'Specific options:'
@@ -10,12 +15,13 @@ option :action do
       short '-a'
       long '--action=ACTION'
       desc 'Specify the action -- add or delete'
-      default 'enable'
+      valid %w[add delete]
     end
 option :filter do
 	short '-f'
 	long '--filter=FILTER'
 	desc 'Specify the filter -- header,body,subject etc...'
+	valid %w[header body subject]
     end
 option :keyword do
 	short '-k'
@@ -32,4 +38,42 @@ option :score do
       desc 'shows this message'
     end
 end
-puts 'action: ' + Choice.choices[:action]
+
+##### Checking the Files #####
+if File.exists?(@filename)
+	puts "file exists"
+else
+	puts "file doesn't exist, so creating the file"
+	File.new("#{@filename}", File::CREAT|File::TRUNC|File::RDWR, 0644)
+end
+if File.read(@sa_conf).include?('thor')
+	puts "file is included"
+else
+	tempfile=File.open("/tmp/file.tmp", 'w')
+	f=File.new(@sa_conf)
+	f.each do |line|
+	      	tempfile<<line
+		    if line =~/^include/
+			     tempfile << "include #{@filename}\n"
+		  end
+	end
+	f.close
+	tempfile.close
+
+	FileUtils.mv("/tmp/file.tmp", @sa_conf)
+end
+##### Starting Attack #####
+if Choice.choices[:action] == 'add'
+	puts 'add'
+   if Choice.choices[:filter] == nil
+	   puts 'Please mention the filter'
+   end
+   if Choice.choices[:keyword] == nil
+           puts 'Please mention the keyword/regexp'
+   end
+   if Choice.choices[:score] == nil
+           puts 'Please mention the score'
+   end
+else
+	puts 'delete'
+end
